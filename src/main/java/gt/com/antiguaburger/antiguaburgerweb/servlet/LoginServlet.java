9955c7c0e97c246092a071742abd832d51811183
ronald <ottoniel.21@outlook.es>
2020-09-12 01:31:43 -0600

    Creacion del LoginServlet

---
+++ src/main/java/gt/com/antiguaburger/antiguaburgerweb/servlet/LoginServlet.java
@@ -0,0 +1,29 @@
+package gt.com.antiguaburger.antiguaburgerweb.servlet;
+
+import com.google.gson.Gson;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    String user;
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        user = request.getParameter("user");
+        response.sendRedirect("/antiguaBweb-1.0-SNAPSHOT/menu.html");
+
+    }
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        try (PrintWriter out = response.getWriter()){
+            out.println(user);
+            System.out.println(user);
+        }
+
+    }
+}
