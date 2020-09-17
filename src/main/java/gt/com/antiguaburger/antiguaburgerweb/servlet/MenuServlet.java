d6453722af6aea843f4a82d523ebe5e6118317f2
ronald <ottoniel.21@outlook.es>
2020-09-17 16:18:07 -0600

    Correccion y eliminacion de las variables globales

--- src/main/java/gt/com/antiguaburger/antiguaburgerweb/servlet/MenuServlet.java
+++ src/main/java/gt/com/antiguaburger/antiguaburgerweb/servlet/MenuServlet.java
@@ -18,37 +18,40 @@ import java.util.List;
 
 @WebServlet("/menuServlet")
 public class MenuServlet extends HttpServlet {
-    OrderWebEntity web = new OrderWebEntity();
-    OrderEntity order = new OrderEntity();
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        OrderWebEntity web = new OrderWebEntity();
+        OrderEntity order = new OrderEntity();
         List<String> option = new ArrayList<>();
         List<String> copt = new ArrayList<>();
         List<String> ext = new ArrayList<>();
         List<String> cext = new ArrayList<>();
 
-
         String cashier = request.getParameter("cashier");
         String id = request.getParameter("idmenu");
         String customer = request.getParameter("customer");
         String taxid = request.getParameter("taxid");
+
         option.add(request.getParameter("opt1"));
         option.add(request.getParameter("opt2"));
         option.add( request.getParameter("opt3"));
-        copt.add(request.getParameter("copt1"));
-        copt.add( request.getParameter("copt2"));
-        copt.add(request.getParameter("copt3"));
+        option.add( request.getParameter("opt4"));
+        option.add( request.getParameter("opt5"));
         ext.add(request.getParameter("extra1"));
-        ext.add( request.getParameter("extra2"));
-        ext.add( request.getParameter("extra3"));
         cext.add( request.getParameter("cextra1"));
+        ext.add(request.getParameter("extra2"));
         cext.add( request.getParameter("cextra2"));
+        ext.add(request.getParameter("extra3"));
         cext.add( request.getParameter("cextra3"));
-
+        ext.add(request.getParameter("extra4"));
+        cext.add( request.getParameter("cextra4"));
+        ext.add(request.getParameter("extra5"));
+        cext.add( request.getParameter("cextra5"));
+        ext.add(request.getParameter("extra6"));
+        cext.add( request.getParameter("cextra6"));
 
         web.setIdmenu(id);
         web.setIdoption(option);
-        web.setCantoption(copt);
         web.setIdExtra(ext);
         web.setCantextra(cext);
         web.setCashier(cashier);
@@ -56,7 +59,7 @@ public class MenuServlet extends HttpServlet {
         web.setUser(customer);
 
         ComboBuilder cm = new ComboBuilder();
-    try {
+         try {
             order = cm.buildc(web);
         } catch (SQLException throwables) {
             throwables.printStackTrace();
@@ -64,12 +67,14 @@ public class MenuServlet extends HttpServlet {
         Gson gson = new Gson();
         String data = gson.toJson(order);
         System.out.println(data);
+        request.getSession(true).setAttribute("order",order);
+
         response.sendRedirect("/antiguaBweb-1.0-SNAPSHOT/bill.html");
     }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         try (PrintWriter out = response.getWriter()){
             Gson gson = new Gson();
-            String data = gson.toJson(order);
+            String data = gson.toJson(request.getSession(false).getAttribute("order"));
             out.println(data);
         }
     }
