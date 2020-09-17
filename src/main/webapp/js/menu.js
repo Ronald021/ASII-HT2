3ac167e78788e3885ba164bc473514decd0a54ea
ronald <ottoniel.21@outlook.es>
2020-09-17 16:30:01 -0600

    Correccion en el script al crear archiv html y creacion de los estilos para los botones descargar

--- src/main/webapp/js/menu.js
+++ src/main/webapp/js/menu.js
@@ -2,9 +2,8 @@ axios.get('http://localhost:7001/antiguaBweb-1.0-SNAPSHOT/loginServlet')
     .then(response => {
         console.log(response.data);
         document.getElementById("demo").innerHTML = "<label>Cashier</label>" +
-            "<input type='text' class='form-group' id='cashier' name='cashier' '"+response.data +"' ><br>";
+            "<input type='text' class='form-control' id='cashier' name='cashier' value=' "+response.data +"' readonly><br>";
     })
     .catch(error => {
         console.error(error)
     });
-
